dee52d378caf886486f5f5928a1716ca11c5800d
idunnoaiii <idunno.aiii@gmail.com>
2022-03-20 17:18:54 +0700

    test crud gorm

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module 200lab
 
 go 1.13
+
+require (
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/text v0.3.7
+	gorm.io/driver/mysql v1.3.2
+	gorm.io/gorm v1.23.3
+)
--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Note struct {
+	Id      int    `json:"id,omitempty" gorm:"column:id;"`
+	Title   string `json:"title,omitempty" gorm:"column:title;"`
+	Content string `json:"content,omitempty" gorm:"column:content;"`
+}
+
+func (Note) TableName() string {
+	return "notes"
+}
+
 func main() {
-	print("hello")
+	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := os.Getenv("DBConnectionString")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+
+	dbConnectionString := os.Getenv("DB_Connection_String")
+
+	db, err := gorm.Open(mysql.Open(dbConnectionString), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("Error open database", err)
+	}
+
+	// newNote := Note{Title: "Demo note", Content:  "This is content of demo not"}
+
+	// if result := db.Create(&newNote); result.Error != nil {
+	// 	fmt.Println(result.Error)
+	// }
+
+	var notes []Note
+	db.Where("status = 1").Find(&notes)
+	fmt.Println(notes)
+
+	db.Table(Note{}.TableName()).Where("id = 3").Delete(nil)
+
 }
